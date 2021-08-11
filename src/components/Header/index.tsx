7e3261ce0c7da3486a339df0e3e163689a5a5aa4
Chiara Campalani <campalanichiara@gmail.com>
2021-08-11 20:00:20 +0900

    added logged-in user info to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context';
 
-const Header: React.FC = () => (
-  <Wrapper >
-    <Content >
-      <Link to='/' >
-        <LogoImg src={RMDBLogo} alt='rmbd-logo' />
-      </Link>
-      <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
-    </Content >
-  </Wrapper >
-)
+const Header: React.FC = () => { 
+//adding logic inside component so changing implicit return brackets to curly brackets & explicit return
+  const [user] = useContext(Context);
+  console.log(user);
+  
+  return (
+    <Wrapper >
+      <Content >
+        <Link to='/' >
+          <LogoImg src={RMDBLogo} alt='rmbd-logo' />
+        </Link>
+        {user ? (
+          <span>Logged in as: {user.username}</span>
+        ) : (
+          <Link to='/login'><span>Log In</span></Link>
+        )}
+        <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+      </Content >
+    </Wrapper >
+  );
+};
 
 export default Header;
